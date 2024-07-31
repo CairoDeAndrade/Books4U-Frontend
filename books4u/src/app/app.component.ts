5e15305de05a1000cb260564ab34e4443ab5d650
cairo <cairo.andrade777@gmail.com>
2024-07-31 20:41:54 -0300

    style(main-menu): Main menu done

--- books4u/src/app/app.component.ts
+++ books4u/src/app/app.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./features/components/navbar/navbar.component";
+
+import { MainMenuComponent } from './features/components/main-menu/main-menu.component';
+import { NavbarComponent } from './features/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, MainMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
