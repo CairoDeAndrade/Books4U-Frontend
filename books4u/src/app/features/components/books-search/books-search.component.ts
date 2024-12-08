bb07dbe8b16da8c23e61dbadd22e0d086a618bc1
Cairo Augusto De Andrade <107778041+CairoDeAndrade@users.noreply.github.com>
2024-12-08 17:41:22 -0300

    feat(books-search): Seraching books by title

--- books4u/src/app/features/components/books-search/books-search.component.ts
+++ books4u/src/app/features/components/books-search/books-search.component.ts
@@ -2,19 +2,35 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
-import { Book } from '../../../model/book/Book';
 import { BookService } from '../../../services/book/book.service';
+import { Book } from '../../../model/book/Book';
+import { APP_BASE_ROUTES } from '../../../common/routes/routes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-books-search',
   standalone: true,
-  imports: [MatIconModule, MatTableModule],
+  imports: [
+    MatIconModule,
+    MatTableModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './books-search.component.html',
   styleUrl: './books-search.component.scss',
 })
 export class BooksSearchComponent {
   books: Book[] = [];
+  filteredBooks: Book[] = [];
+  searchQuery: string = '';
+
   displayedColumns: string[] = [
     'title',
     'publisher',
@@ -24,9 +40,12 @@ export class BooksSearchComponent {
     'actions',
   ];
 
+  APP_BASE_ROUTES = APP_BASE_ROUTES;
+
   constructor(
     private bookService: BookService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -37,16 +56,28 @@ export class BooksSearchComponent {
     this.bookService.getBooks().subscribe({
       next: (data: Book[]) => {
         this.books = data;
+        this.filteredBooks = [...this.books];
       },
       error: () => {
-        this.snackBar.open('Erro ao carregar os livros!', '', {
+        this.snackBar.open('Error loading books!', '', {
           duration: 7000,
         });
       },
     });
   }
 
+  filterBooks(): void {
+    const query = this.searchQuery.toLowerCase().trim();
+    this.filteredBooks = this.books.filter((book) =>
+      book.title.toLowerCase().includes(query)
+    );
+  }
+
   getAuthors(authors: any[]): string {
     return authors.map((author) => author.name).join(', ');
+  }
+
+  navigateTo(path: string): void {
+    this.router.navigate([path]);
   }
 }
